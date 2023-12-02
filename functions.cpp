705bfd09cd1dc03c4091d291449850158817e065
Duane Schmidt <152690675+droids-dopiest@users.noreply.github.com>
2023-12-02 13:01:35 +0300

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "functions.h" // Include the header file to ensure consistency
+
+// Function definition
+void printMessage() {
+    std::cout << "Hello, world!" << std::endl;
+}
